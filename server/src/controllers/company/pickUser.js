70d226c1aeea5f3c2b2b82c83bd949d6a99e0662
Gowtham Punniyaseelan <88957910+gowthampunniyaseelan@users.noreply.github.com>
2022-10-23 10:21:50 +0530

    response changes

--- server/src/controllers/company/pickUser.js
+++ server/src/controllers/company/pickUser.js
@@ -1,6 +1,10 @@
 const CompanyModel = require("../../database/models/company")
 module.exports = function(req,res){
  CompanyModel.findOne({users:{$elemMatch:{email:req.params.id}}},{_id:0,users:1}).then((result)=>{
-  res.status(200).json(result)
+  if(result){
+    res.status(200).json(result)
+  }else{
+    res.status(404).json({message:"No User Available"})
+  }
  })
 }

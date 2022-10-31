2c7b6690740034466d5adef1f4db4e248773dfc9
Gowtham Punniyaseelan <88957910+gowthampunniyaseelan@users.noreply.github.com>
2022-10-31 18:10:40 +0530

    app component changes

--- client/src/App.js
+++ client/src/App.js
@@ -19,24 +19,23 @@ import {
 } from "react-router-dom";
 
 function App() {
-  const temp = window.localStorage.getItem("emailId")
   return(
     <>
     <Router>
     <Routes>
     <Route path="/" element={<SignUp/>}/>
     <Route path="/login" element={<Login/>}/>
-    <Route path="/create-company" element={temp ? <CreateCompany/> : <Navigate to="/"/>}/> 
-    <Route path="/get-company" element={temp ? <GetCompany/> : <Navigate to="/"/>}/>
-    <Route path="/update-company" element={temp ? <UpdateCompany/> : <Navigate to="/"/>}/>
-    <Route path="/list-company" element={temp ? <GenerateListView/>: <Navigate to="/"/>}/>
-    <Route path="/add-user" element={temp ? <AddUser/> : <Navigate to="/"/>}/>
-    <Route path="/remove-user" element={temp ? <RemoveUser/>: <Navigate to="/"/>}/>
-    <Route path="/delete-company" element={temp ?<DeleteCompany/>: <Navigate to="/"/>}/>
-    <Route path="/list-user" element={temp ?<GenerateUserListView/>: <Navigate to="/"/>}/>
-    <Route path="/get-user" element={temp ? <GetUser/>: <Navigate to="/"/>}/>
-    <Route path="/update-user" element={temp ? <UpdateUser/>: <Navigate to="/"/>}/>
-    <Route path="/migrate" element={temp ? <Migrate/>: <Navigate to="/"/> }/>
+    <Route path="/create-company" element={window.localStorage.getItem("emailId") ? <CreateCompany/> : <Navigate to="/"/>}/> 
+    <Route path="/get-company" element={window.localStorage.getItem("emailId") ? <GetCompany/> : <Navigate to="/"/>}/>
+    <Route path="/update-company" element={window.localStorage.getItem("emailId") ? <UpdateCompany/> : <Navigate to="/"/>}/>
+    <Route path="/list-company" element={window.localStorage.getItem("emailId") ? <GenerateListView/>: <Navigate to="/"/>}/>
+    <Route path="/add-user" element={window.localStorage.getItem("emailId") ? <AddUser/> : <Navigate to="/"/>}/>
+    <Route path="/remove-user" element={window.localStorage.getItem("emailId") ? <RemoveUser/>: <Navigate to="/"/>}/>
+    <Route path="/delete-company" element={window.localStorage.getItem("emailId") ?<DeleteCompany/>: <Navigate to="/"/>}/>
+    <Route path="/list-user" element={window.localStorage.getItem("emailId") ?<GenerateUserListView/>: <Navigate to="/"/>}/>
+    <Route path="/get-user" element={window.localStorage.getItem("emailId") ? <GetUser/>: <Navigate to="/"/>}/>
+    <Route path="/update-user" element={window.localStorage.getItem("emailId") ? <UpdateUser/>: <Navigate to="/"/>}/>
+    <Route path="/migrate" element={window.localStorage.getItem("emailId") ? <Migrate/>: <Navigate to="/"/> }/>
     </Routes>
     </Router> 
     </>
